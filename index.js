42652b0e1f5bc9b7197e0615c7e6e30410b7445f
Haruo Taninaka <haruoborder32@gmail.com>
2020-12-15 21:02:57 +0900

    コードを整形

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 function _onSubmit(e) {
     const query = e.target.elements['q'].value;
-    if(!query) {
+    if (!query) {
         window.alert('本のタイトルを入力してください。');
         e.preventDefault(); // formタグのSubmitを止める
     }
 }
- 
